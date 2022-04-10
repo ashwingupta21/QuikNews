7e0998c1b825f67bae24f8c59ef2f0d63cea242b
Wafi Choudhury <wafster1337@gmail.com>
2022-04-09 19:23:16 -0500

    Initial Commit

---
+++ QuikNews/QuikNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuikNewsApp.swift
+//  QuikNews
+//
+//  Created by Wafi Choudhury on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct QuikNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
